6ae9460961047272514a584f9f8ff5f7034c8ad2
Azuka Ehi <ehi.azuka@gmail.com>
2022-08-10 06:51:26 -0700

    goal controller and routes set

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
-const express = require('express');
- const app = express();
-  app.get('/', (req, res) => {
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.get('/', (req, res) => {
      res.json({ message: 'Hello there!', }); });
-      app.listen(3001, () => {
-         console.log('Server is running'); });
+
+
+app.listen(port, () => {
+         console.log('Server is running on port '+ port) });
